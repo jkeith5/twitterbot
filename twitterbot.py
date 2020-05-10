58bee162df3fdbf85ad13d7040604827d855e0e9
jkeith5 <32349572+jkeith5@users.noreply.github.com>
2020-05-09 21:29:24 -0500

    rough draft using tweepy and pypi for twitterbot that posts random wiki pages each day

---
+++ twitterbot.py
@@ -0,0 +1,83 @@
+import tweepy
+import wikipedia
+from datetime import datetime as dt
+import schedule
+import time
+import twitterkeys as tk
+import requests
+
+execTime = "20:36"
+listOfPages = []
+
+S = requests.Session()
+URL = "https://en.wikipedia.org/w/api.php"
+PARAMS = {
+    "action": "query",
+    "rnnamespace":"0",
+    "format": "json",
+    "list": "random",
+    "rnlimit": "1"
+}
+
+def urlConstruction(
+
+def wikiRandomCall():
+    
+
+    R = S.get(url=URL, params=PARAMS)
+    DATA = R.json()
+    RANDOMS = DATA["query"]["random"]
+    for r in RANDOMS:
+        if (r["id"]):
+            titlestring = (r["title"])
+        print(titlestring)
+    
+    
+    S.close()
+    
+
+wikiRandomCall()
+
+def job():
+    print ("I'm working...")
+    now= dt.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current time = ", current_time)
+
+    consumer_key = tk.consumer_key
+    consumer_secret = tk.consumer_secret
+    access_token = tk.access_token
+    access_token_secret = tk.access_token_secret
+
+    #auth to twitter
+    auth = tweepy.OAuthHandler(consumer_key,
+                      consumer_secret)
+    auth.set_access_token(access_token,
+                      access_token_secret)
+
+    api = tweepy.API(auth)
+
+    try:
+        redirect_url = auth.get_authorization_url()
+    except tweepy.TweepError:
+        print('Error! Failed to get request token.')
+
+    #get the random wiki page
+    
+
+
+    #below posts a tweet
+    api = tweepy.API(auth)
+    api.update_status("it is: " + execTime)
+    return
+
+schedule.every().day.at(execTime).do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
+
+    
+
+
+
